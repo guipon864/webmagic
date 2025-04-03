1090d4d21147fe22bc7074fa8522f42d6f6008ca
Guillaume Ponsdesserre <guillaume.ponsdesserre.etu@univ-lille.fr>
2025-04-03 14:07:42 +0200

    correction de test

--- webmagic-extension/src/test/java/us/codecraft/webmagic/processor/GithubRepoProcessor.java
+++ webmagic-extension/src/test/java/us/codecraft/webmagic/processor/GithubRepoProcessor.java
@@ -1,8 +1,12 @@
 package us.codecraft.webmagic.processor;
 
-import junit.framework.Assert;
 import org.junit.Test;
-import us.codecraft.webmagic.*;
+
+import junit.framework.Assert;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.downloader.MockGithubDownloader;
 import us.codecraft.webmagic.model.OOSpider;
 import us.codecraft.webmagic.pipeline.Pipeline;
@@ -13,8 +17,14 @@ import us.codecraft.webmagic.pipeline.Pipeline;
 public class GithubRepoProcessor implements PageProcessor {
     @Override
     public void process(Page page) {
-        page.putField("star",page.getHtml().xpath("//ul[@class='pagehead-actions']/li[2]//a[@class='social-count js-social-count']/text()").toString());
-        page.putField("fork",page.getHtml().xpath("//ul[@class='pagehead-actions']/li[3]//a[@class='social-count']/text()").toString());
+        //page.putField("star",page.getHtml().xpath("//ul[@class='pagehead-actions']/li[2]//a[@class='social-count js-social-count']/text()").toString());
+        //page.putField("fork",page.getHtml().xpath("//ul[@class='pagehead-actions']/li[3]//a[@class='social-count']/text()").toString());
+        String star = page.getHtml().css("a[href$='stargazers']", "text").get();
+        String fork = page.getHtml().css("a[href$='network']", "text").get();
+        star = star.trim().replaceAll("[^0-9]", "");
+        fork = fork.trim().replaceAll("[^0-9]", "");
+        page.putField("star", star);
+        page.putField("fork", fork);
     }
 
     @Override
@@ -27,8 +37,8 @@ public class GithubRepoProcessor implements PageProcessor {
         OOSpider.create(new GithubRepoProcessor()).addPipeline(new Pipeline() {
             @Override
             public void process(ResultItems resultItems, Task task) {
-                Assert.assertEquals("78",((String)resultItems.get("star")).trim());
-                Assert.assertEquals("65",((String)resultItems.get("fork")).trim());
+                Assert.assertEquals("86",((String)resultItems.get("star")).trim()); // 86 est le nombre défini dans le html de MockGithubDownloader
+                Assert.assertEquals("70",((String)resultItems.get("fork")).trim()); // 70 est le nombre défini dans le html de MockGithubDownloader
             }
         }).setDownloader(new MockGithubDownloader()).test("https://github.com/code4craft/webmagic");
     }
