0964b2d8a6c70f3f54121bf7eebb7e18eacd7d13
Guillaume Ponsdesserre <guillaume.ponsdesserre.etu@univ-lille.fr>
2025-04-05 22:47:19 +0200

    javadoc

--- webmagic-extension/src/main/java/us/codecraft/webmagic/model/ModelPageProcessor.java
+++ webmagic-extension/src/main/java/us/codecraft/webmagic/model/ModelPageProcessor.java
@@ -19,12 +19,28 @@ import java.util.regex.Pattern;
  */
 class ModelPageProcessor implements PageProcessor {
 
+    /**
+     * The list of page model extractor.
+     */
     private List<PageModelExtractor> pageModelExtractorList = new ArrayList<PageModelExtractor>();
 
+    /**
+     * The site of this page processor.
+     */
     private Site site;
 
+    /**
+     * Whether to extract links from the page.
+     */
     private boolean extractLinks = true;
 
+    /**
+     * Create a ModelPageProcessor with the given site and page model classes.
+     *
+     * @param site   the site
+     * @param clazzs the page model classes
+     * @return the ModelPageProcessor instance
+     */
     public static ModelPageProcessor create(Site site, Class... clazzs) {
         ModelPageProcessor modelPageProcessor = new ModelPageProcessor(site);
         for (Class clazz : clazzs) {
@@ -33,17 +49,30 @@ class ModelPageProcessor implements PageProcessor {
         return modelPageProcessor;
     }
 
-
+    /**
+     * Add a page model class to the processor.
+     *
+     * @param clazz the page model class
+     * @return the ModelPageProcessor instance
+     */
     public ModelPageProcessor addPageModel(Class clazz) {
         PageModelExtractor pageModelExtractor = PageModelExtractor.create(clazz);
         pageModelExtractorList.add(pageModelExtractor);
         return this;
     }
 
+    /**
+     * set the site of this page processor.
+     * @param site
+     */
     private ModelPageProcessor(Site site) {
         this.site = site;
     }
 
+    /**
+     * Process the page.
+     * @param page page
+     */
     @Override
     public void process(Page page) {
         for (PageModelExtractor pageModelExtractor : pageModelExtractorList) {
@@ -63,6 +92,12 @@ class ModelPageProcessor implements PageProcessor {
         }
     }
 
+    /**
+     * Extract links from the page.
+     * @param page page
+     * @param urlRegionSelector the selector for the region to extract links from
+     * @param urlPatterns the patterns to match the links
+     */
     private void extractLinks(Page page, Selector urlRegionSelector, List<Pattern> urlPatterns) {
         List<String> links;
         if (urlRegionSelector == null) {
@@ -80,18 +115,35 @@ class ModelPageProcessor implements PageProcessor {
         }
     }
 
+    /**
+     * Post process the page model.
+     * @param clazz the class of the page model
+     * @param object the page model object
+     */
     protected void postProcessPageModel(Class clazz, Object object) {
     }
 
+    /**
+     * Get the site of this page processor.
+     * @return site
+     */
     @Override
     public Site getSite() {
         return site;
     }
 
+    /**
+     * get if the links should be extracted.
+     * @return true if links should be extracted, false otherwise
+     */
     public boolean isExtractLinks() {
         return extractLinks;
     }
 
+    /**
+     * set if the links should be extracted.
+     * @param extractLinks true if links should be extracted, false otherwise
+     */
     public void setExtractLinks(boolean extractLinks) {
         this.extractLinks = extractLinks;
     }
